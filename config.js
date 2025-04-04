d2a97c737525ba1510a524a8348e0e84dce3f31d
CyniBot <150451220+CyniBot@users.noreply.github.com>
2025-04-04 11:35:34 +0800

    Update and rename config.js.example to config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+"use strict"
+exports.ConfigSettings = {
+    servers: [
+        {
+            listenPort: 7777,
+            routingServers: [{
+                name: "lobby",
+                serverIP: "116.202.48.240",
+                serverPort: 7059,
+                hidden: false,
+            }]    
+        },
+    ],
